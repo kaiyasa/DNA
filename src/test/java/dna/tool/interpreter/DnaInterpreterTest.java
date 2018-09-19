cae1d5aca07c0e5e64c372a677d095e8087bbd24
Dan Miner <dminer84@yahoo.com>
2018-09-18 20:40:48 -0600

    testing with two variables

--- src/test/java/dna/tool/interpreter/DnaInterpreterTest.java
+++ src/test/java/dna/tool/interpreter/DnaInterpreterTest.java
@@ -9,6 +9,7 @@ import dna.antlr.ValidationException;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.fail;
 
 public class DnaInterpreterTest extends DnaParseTestBase<Void, XUnitContext, DnaInterpreter> {
 	public DnaInterpreterTest() {
@@ -23,8 +24,8 @@ public class DnaInterpreterTest extends DnaParseTestBase<Void, XUnitContext, Dna
 		assertThat(item.typeInfo.kind(), equalTo(TypeInfo.Kind.INT));
 		assertThat(item.name, equalTo("a"));
 	}
-	
-	@Test(expected=ValidationException.class)
+
+	@Test(expected = ValidationException.class)
 	public void oneVariableAsStruct() {
 		actOn("Data a");
 
@@ -32,7 +33,7 @@ public class DnaInterpreterTest extends DnaParseTestBase<Void, XUnitContext, Dna
 		assertThat(item.typeInfo.kind(), equalTo(TypeInfo.Kind.INT));
 		assertThat(item.name, equalTo("a"));
 	}
-	
+
 	@Test
 	public void oneVariableAsString() {
 		actOn("string a");
@@ -42,5 +43,28 @@ public class DnaInterpreterTest extends DnaParseTestBase<Void, XUnitContext, Dna
 		assertThat(item.name, equalTo("a"));
 	}
 
-	private Variable item ;
+	@Test
+	public void twoVariables() {
+		actOn("int b, c");
+
+		item = visitor.variable("b");
+		assertThat(item.name, equalTo("b"));
+		assertThat(item.typeInfo.kind(), equalTo(TypeInfo.Kind.INT));
+
+		item = visitor.variable("c");
+		assertThat(item.name, equalTo("c"));
+		assertThat(item.typeInfo.kind(), equalTo(TypeInfo.Kind.INT));
+	}
+
+	@Test
+	public void redefineVariable() {
+		try {
+			actOn("int b \n string b");
+			fail("exception expected");
+		} catch (ValidationException e) {
+			assertThat(e.getMessage(), containsString("redefinition of variable"));
+		}
+	}
+
+	private Variable item;
 }
